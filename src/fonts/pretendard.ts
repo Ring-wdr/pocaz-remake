b1045e3f50463f9930f42d84ff9396f95ef9ea80
ring-wdr <akswnd55@gmail.com>
2025-12-01 20:43:58 +0900

    fix: cdn font to local font

---
+++ src/fonts/pretendard.ts
@@ -0,0 +1,29 @@
+import localFont from "next/font/local";
+
+export const pretendard = localFont({
+	src: [
+		{
+			path: "../../public/fonts/pretendard/Pretendard-Regular.woff2",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "../../public/fonts/pretendard/Pretendard-Medium.woff2",
+			weight: "500",
+			style: "normal",
+		},
+		{
+			path: "../../public/fonts/pretendard/Pretendard-Bold.woff2",
+			weight: "700",
+			style: "normal",
+		},
+		{
+			path: "../../public/fonts/pretendard/Pretendard-Black.woff2",
+			weight: "900",
+			style: "normal",
+		},
+	],
+	display: "swap",
+	fallback: ["Noto Sans KR", "Apple SD Gothic Neo", "system-ui", "sans-serif"],
+	variable: "--font-pretendard",
+});
